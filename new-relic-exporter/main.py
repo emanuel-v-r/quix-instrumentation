50b345a384d301324d678046e93d3ac5a03dad07
Emanuel Ramos <emanuel+hackaton@quix.io>
2024-02-08 23:44:14 +0000

    Edited 'main.py'

--- new-relic-exporter/main.py
+++ new-relic-exporter/main.py
@@ -9,4 +9,29 @@ client = qx.QuixStreamingClient()
 consumer_topic = client.get_topic_consumer(os.environ["input"])
 producer_topic = client.get_topic_producer(os.environ["output"])
 
+
+import requests
+import json
+import time
+
+url = "https://metric-api.newrelic.com/metric/v1"
+headers = {
+    "Content-Type": "application/json",
+    "Api-Key": "NEW_RELIC_LICENSE_KEY"
+}
+
+data = [{
+    "metrics": [{
+        "name": "memory.heap",
+        "type": "gauge",
+        "value": 2.3,
+        "timestamp": int(time.time()),  # Assuming you want current Unix timestamp
+        "attributes": {"host.name": "dev.server.com"}
+    }]
+}]
+
+response = requests.post(url, headers=headers, data=json.dumps(data), verify=False)
+
+print("Response code:", response.status_code)
+print("Response content:", response.content)
 # for more samples, please see samples or docs
