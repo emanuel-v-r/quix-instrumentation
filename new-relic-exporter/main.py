ed4d722ab216f5f63ffd114f708bb7cbbc9550a5
Emanuel Ramos <emanuel+hackaton@quix.io>
2024-02-09 11:49:33 +0000

    Edited 'main.py'

--- new-relic-exporter/main.py
+++ new-relic-exporter/main.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 import json
 import logging
 import sys
+from concurrent.futures import ThreadPoolExecutor
 
 load_dotenv()
 
@@ -14,6 +15,23 @@ logger = logging.getLogger(__name__)
 
 metrics_endpoint = os.environ.get("NEWRELIC_ENDPOINT")
 metrics_key = os.environ.get("NEWRELIC_KEY")
+
+def process_message(payload):
+    try:
+        if metrics_endpoint and metrics_key:
+            url = metrics_endpoint
+            headers = {
+                "Content-Type": "application/json",
+                "Api-Key": metrics_key
+            }
+
+            response = requests.post(url, headers=headers, data=payload)
+            logger.info("Response code: %s", response.status_code)
+            logger.info("Response content: %s", response.content)
+        else:
+            logger.error("Metrics endpoint or key not found in environment variables.")
+    except Exception as e:
+        logger.error("Error processing message: %s", str(e))
 
 def main():
     try:
@@ -27,27 +45,16 @@ def main():
             input_topic = app.topic(os.environ["input"]).name
             consumer.subscribe([input_topic])
             logger.info("Waiting for messages...")
-            while True:
-                msg = consumer.poll(timeout=1.0)
-                if msg is not None:
-                    try:
-                        payload = msg.value().decode('utf-8')
-                        logger.info("Received message: %s", payload)
-
-                        if metrics_endpoint and metrics_key:
-                            url = metrics_endpoint
-                            headers = {
-                                "Content-Type": "application/json",
-                                "Api-Key": metrics_key
-                            }
-
-                            response = requests.post(url, headers=headers, data=payload)
-                            logger.info("Response code: %s", response.status_code)
-                            logger.info("Response content: %s", response.content)
-                        else:
-                            logger.error("Metrics endpoint or key not found in environment variables.")
-                    except Exception as e:
-                        logger.error("Error processing message: %s", str(e))
+            with ThreadPoolExecutor(max_workers=10) as executor:  # Adjust max_workers as needed
+                while True:
+                    msg = consumer.poll(timeout=1.0)
+                    if msg is not None:
+                        try:
+                            payload = msg.value().decode('utf-8')
+                            logger.info("Received message: %s", payload)
+                            executor.submit(process_message, payload)
+                        except Exception as e:
+                            logger.error("Error processing message: %s", str(e))
     except KeyboardInterrupt:
         logger.info("Received keyboard interrupt. Exiting...")
     except Exception as e:
